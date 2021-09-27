c9192e0bdd194f45a5f88c919044acd1ea521f5f
root <root@debian.debgns3.local>
2021-09-26 22:22:01 -0600

    parte final de Edwin Diaz

--- wordpress/src/wp-config.php
+++ wordpress/src/wp-config.php
@@ -49,6 +49,8 @@ define( 'DB_USER', getenv_docker('WORDPRESS_DB_USER', 'example username') );
 /** MySQL database password */
 define( 'DB_PASSWORD', getenv_docker('WORDPRESS_DB_PASSWORD', 'example password') );
 
+/*FS_METHOD */
+define( 'FS_METHOD', getenv_docker('FS_METHOD', 'direct') );
 /**
  * Docker image fallback values above are sourced from the official WordPress installation wizard:
  * https://github.com/WordPress/WordPress/blob/f9cc35ebad82753e9c86de322ea5c76a9001c7e2/wp-admin/setup-config.php#L216-L230
